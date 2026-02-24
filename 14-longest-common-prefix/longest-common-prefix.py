71695b1f5e8bfb7e727e14c02541663379e72847
Mugilan <expectopetronum002@gmail.com>
2026-02-24 14:57:26 +0530

    Time: 5 ms (7.50%) | Memory: 12.4 MB (65.86%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        res=""
+        for i in range(len(strs[0])):
+            print(i)
+            for s in strs:
+                if i== len(s) or s[i] != strs[0][i]:
+                    return res
+            res+=strs[0][i]
+            print("res :"+res)
+        return res
+        
