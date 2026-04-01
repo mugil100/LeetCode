d5c476b47dbecf46d6dfa394fd0d733302ef4e32
Mugilan <expectopetronum002@gmail.com>
2026-04-01 23:50:26 +0530

    Time: 14 ms (32.00%) | Memory: 47.4 MB (13.26%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        String rev = new StringBuilder(s).reverse().toString();
+        return s.equals(rev); 
+    }
+}
