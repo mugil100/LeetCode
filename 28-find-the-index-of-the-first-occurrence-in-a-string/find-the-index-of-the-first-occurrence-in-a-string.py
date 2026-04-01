e5d096f8b0489234b8592117ffd7cc2308a316ed
Mugilan <expectopetronum002@gmail.com>
2026-04-01 14:31:51 +0530

    Time: 4 ms (16.66%) | Memory: 12.4 MB (89.56%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -9,4 +9,9 @@ class Solution(object):
         for i in range(len(haystack)-len(needle)+1):
             if haystack[i: i+len(needle)]== needle:
                 return i
+            # for j in range(len(needle)):
+            #     if haystack[i+j] != needle[j]:
+            #         break
+            #     if j== len(needle)-1:
+            #         return i
         return -1
