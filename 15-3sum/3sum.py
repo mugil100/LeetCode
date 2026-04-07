e02ccef13f5d9a16387898b019151d1071730c8b
Mugilan <expectopetronum002@gmail.com>
2026-04-07 17:38:37 +0530

    Time: 2845 ms (11.21%) | Memory: 18.1 MB (97.67%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def threeSum(self, nums):
         nums.sort()
-        res=[]
+        res=set()
         for i in range(len(nums)):
             j=i+1
             k= len(nums)-1
             while j<k:
-                if nums[i] + nums[j] + nums[k] == 0 and [nums[i] , nums[j] , nums[k]] not in res:
-                    res.append([nums[i] , nums[j] , nums[k]])
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.add(tuple([nums[i] , nums[j] , nums[k]]))
                     j+=1
                     k-=1
                 elif nums[i] + nums[j] + nums[k] >0:
                     k-=1
                 else:
                     j+=1
-        return res
+        return list(res)
 
