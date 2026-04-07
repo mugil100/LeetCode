83a2146dd8abeb45a9ce3ade309b49e81999f8b8
Mugilan <expectopetronum002@gmail.com>
2026-04-07 23:09:07 +0530

    Time: 1071 ms (20.18%) | Memory: 18.1 MB (94.90%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,6 +3,7 @@ class Solution(object):
         nums.sort()
         res=set()
         for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]: continue
             j=i+1
             k= len(nums)-1
             while j<k:
@@ -10,6 +11,8 @@ class Solution(object):
                     res.add(tuple([nums[i] , nums[j] , nums[k]]))
                     j+=1
                     k-=1
+                    while j<k and nums[j]==nums[j-1]: j+=1
+                    while j<k and nums[k]==nums[k+1]: k-=1
                 elif nums[i] + nums[j] + nums[k] >0:
                     k-=1
                 else:
