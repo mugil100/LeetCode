3e65baa0f35cffb47d7b84229571849acc00e871
Mugilan <expectopetronum002@gmail.com>
2026-04-29 23:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.88%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        res=[]
+        l,r = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        
+        while l <r and top <bottom:
+            for i in range(l,r):
+                res.append(matrix[top][i])# eles from every top row
+            top+=1
+
+            for i in range(top, bottom):
+                res.append(matrix[i][r-1])
+            r-=1
+
+            if not(l<r and top<bottom): break
+
+            for i in range(r-1,l-1,-1):
+                res.append(matrix[bottom-1][i])
+            bottom-=1
+
+            for i in range(bottom-1,top-1,-1):
+                res.append(matrix[i][l])
+            l+=1
+        return res
+
+        
