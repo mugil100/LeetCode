9e3d24fdaf60456502807e2dfdc9fda7f0ecf2e2
Mugilan <expectopetronum002@gmail.com>
2026-05-07 20:57:38 +0530

    Time: 19 ms (61.50%) | Memory: 19.6 MB (73.09%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,18 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         r = {}
+        m = {}
         count=0
         for char in ransomNote:
-            if char in r:
-                r[char]+=1
-            else:
-                r[char]=1
-        m = {}
+            r[char]=r.get(char,0)+1
+
         for char in magazine:
-            if char in m:
-                m[char]+=1
-            else:
-                m[char]=1
+            m[char]=m.get(char,0)+1
+
         for key,value in m.items():
             if key in r and r[key]<=value:
                 count+=1
