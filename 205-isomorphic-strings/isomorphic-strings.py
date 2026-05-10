2414c9513c18e1d3c331994968f055206b7a55a5
Mugilan <expectopetronum002@gmail.com>
2026-05-10 13:54:00 +0530

    Time: 3 ms (91.51%) | Memory: 19.3 MB (24.42%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,7 +5,9 @@ class Solution:
         for i in range(len(s)):
             val=s[i]
             if val  in mapper:
-                if mapper[val]!=t[i]:
+                if mapper[val]==t[i]:
+                    continue
+                else:
                     return False
             else:
                 if t[i] in used:
