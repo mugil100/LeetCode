e3be4a5ca4400bb68237dfcfc0cfb1c945df8c36
Mugilan <expectopetronum002@gmail.com>
2026-05-10 19:52:39 +0530

    Time: 11 ms (84.39%) | Memory: 22.2 MB (57.20%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,10 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         mapper = defaultdict(list)
-
         for s in strs:
             sval = "".join(sorted(s))
-            print(sval)
             mapper[sval].append(s)
         return list(mapper.values())
 
