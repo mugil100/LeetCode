4899479553473cce1d88f52c67f5a60ddc788d9c
Mugilan <expectopetronum002@gmail.com>
2026-05-10 13:53:29 +0530

    Time: 4 ms (57.89%) | Memory: 19.2 MB (82.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,21 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapper = {}
-        used = set()
+        mapper = defaultdict()
+        used= set()
         for i in range(len(s)):
-            val = s[i]
-            if val in mapper :
-                if mapper[val]==t[i]:
-                    continue
-                else:
+            val=s[i]
+            if val  in mapper:
+                if mapper[val]!=t[i]:
                     return False
             else:
-                mapper[s[i]]=t[i]
                 if t[i] in used:
                     return False
+                mapper[val]=t[i]
                 used.add(t[i])
         return True
-        
-
-
         
