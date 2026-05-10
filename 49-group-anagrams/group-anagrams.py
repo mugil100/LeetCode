6bd32670cff5ba1133afb4ec9d110a105219dead
Mugilan <expectopetronum002@gmail.com>
2026-05-10 19:53:30 +0530

    Time: 3 ms (100.00%) | Memory: 22 MB (80.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,6 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
         mapper = defaultdict(list)
         for s in strs:
             sval = "".join(sorted(s))
