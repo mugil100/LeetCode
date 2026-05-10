8e13d2bf07b6468328cbe06768c807f0a95b9994
Mugilan <expectopetronum002@gmail.com>
2026-05-10 14:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (29.08%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,21 +1,11 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words=[]
+        words= s.split()
         f=None
         l=None
         n=len(s)
         mapper={}
-        used = set()
-        for i in range(n):
-            if s[i]!=" ":
-                if i==0 or s[i-1]==" ":
-                    f=i
-                if i==n-1 or s[i+1]==" ":
-                    l=i
-                if f is not None and l is not None:
-                    words.append(s[f:l+1])
-                    f=None
-                    l=None
+        used = {}
         if len(words)!= len(pattern):
             return False
         for i in range(len(pattern)):
@@ -29,7 +19,7 @@ class Solution:
                 if words[i] in used:
                     return False
                 mapper[val]=words[i]
-                used.add(words[i])
+                used[words[i]]=val
         return True
 
 
