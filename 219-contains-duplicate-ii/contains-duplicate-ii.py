6fbac05ae3a8a2fead64014b75a076f765dd0ca0
Mugilan <expectopetronum002@gmail.com>
2026-05-11 09:55:04 +0530

    Time: 31 ms (70.03%) | Memory: 36.8 MB (17.19%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -3,10 +3,8 @@ class Solution:
         n = len(nums)
         hashk = {}
         for i in range(n):
-            if nums[i] in hashk:
-                tgt = abs(i-hashk[nums[i]])
-                if tgt<=k:
-                    return True
+            if nums[i] in hashk and abs(i-hashk[nums[i]])<=k:
+                return True
             hashk[nums[i]]=i
         return False
         
