0ff2890fb65b0d170b4a3432f8d74d13192a4033
Mugilan <expectopetronum002@gmail.com>
2026-05-17 00:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (33.03%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,22 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # 1. Create a dummy node to act as the start of the merged list
-        dummy = ListNode(-1)
+
+        dummy = ListNode()
         curr = dummy
-        
-        # 2. Loop while BOTH lists have nodes remaining
         while list1 and list2:
             if list1.val <= list2.val:
-                curr.next = list1  # Splice the actual node, don't create a new one
-                list1 = list1.next # Move list1 pointer forward
+                curr.next = ListNode(list1.val)
+                list1 = list1.next
+                curr = curr.next
             else:
-                curr.next = list2  # Splice list2 node
-                list2 = list2.next # Move list2 pointer forward
-            
-            curr = curr.next       # Crucial: Move your tracking pointer forward!
-            
-        # 3. Attach the remaining nodes of whichever list isn't empty
-        curr.next = list1 if list1 else list2
-        
-        # 4. Return the head of the sorted list (skipping the dummy node)
+                curr.next = ListNode(list2.val)
+                list2 = list2.next
+                curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+
         return dummy.next
+
+
+
+        
