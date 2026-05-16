d305fbd142bb42249d5aa329bc72ed3eec8f0221
Mugilan <expectopetronum002@gmail.com>
2026-05-16 15:51:07 +0530

    Time: 37 ms (65.80%) | Memory: 20.2 MB (87.86%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -5,26 +5,25 @@ class Solution:
         stack =[]
 
         for x in s:
-            if x!=" ":
-                if x.isdigit():
-                    curr = curr*10+int(x)
-                elif x in ["+","-"]:
-                    res += curr*sign
-                    sign = 1 if x=="+" else -1
-                    curr = 0
+            if x.isdigit():
+                curr = curr*10+int(x)
+            elif x in ["+","-"]:
+                res += curr*sign
+                sign = 1 if x=="+" else -1
+                curr = 0
 
-                elif x=="(":
-                    stack.append(res)
-                    stack.append(sign)
-                    res=0
-                    sign=1
+            elif x=="(":
+                stack.append(res)
+                stack.append(sign)
+                res=0
+                sign=1
 
-                elif x==")":
+            elif x==")":
 
-                    res += sign*curr 
-                    res *= stack.pop()
-                    res += stack.pop()
-                    curr = 0
+                res += sign*curr 
+                res *= stack.pop()
+                res += stack.pop()
+                curr = 0
 
         return res + curr*sign
 
