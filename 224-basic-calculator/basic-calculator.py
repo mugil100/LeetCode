2715e387864eea91716eaed3debf68da1eb58c15
Mugilan <expectopetronum002@gmail.com>
2026-05-16 15:50:11 +0530

    Time: 37 ms (65.80%) | Memory: 20.3 MB (63.77%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        res = curr = 0
+        sign = 1
+        stack =[]
+
+        for x in s:
+            if x!=" ":
+                if x.isdigit():
+                    curr = curr*10+int(x)
+                elif x in ["+","-"]:
+                    res += curr*sign
+                    sign = 1 if x=="+" else -1
+                    curr = 0
+
+                elif x=="(":
+                    stack.append(res)
+                    stack.append(sign)
+                    res=0
+                    sign=1
+
+                elif x==")":
+
+                    res += sign*curr 
+                    res *= stack.pop()
+                    res += stack.pop()
+                    curr = 0
+
+        return res + curr*sign
+
+        
