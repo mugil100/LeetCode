a26dbd2c667a71a2c4399d8c2ad2bc3a852101ce
Mugilan <expectopetronum002@gmail.com>
2026-05-20 00:37:10 +0530

    Time: 3 ms (28.67%) | Memory: 19.2 MB (94.52%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -1,24 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 1. Create a dummy node that points to the head
-        dummy = ListNode(0, head)
-        
-        # 'pred' (predecessor) will stay behind the chunk of duplicates we are inspecting
-        pred = dummy
-        
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev=dummy
+
         while head:
-            # 2. If it's the start of a duplicate sequence
             if head.next and head.val == head.next.val:
-                # Move 'head' to the end of the duplicate sequence
                 while head.next and head.val == head.next.val:
                     head = head.next
-                # Skip all duplicates by linking pred.next to the node AFTER the duplicates
-                pred.next = head.next 
+                prev.next = head.next
             else:
-                # 3. No duplicate detected, safe to move 'pred' forward
-                pred = pred.next
-                
-            # Move head forward for the next iteration
+                prev = prev.next
             head = head.next
-            
+
         return dummy.next
+        
