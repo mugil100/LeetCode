e1151850172c6b5e9d82fb50125cf115b0cf1c71
Mugilan <expectopetronum002@gmail.com>
2026-05-25 00:28:55 +0530

    Time: 1 ms (33.52%) | Memory: 19.6 MB (17.41%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,15 +1,15 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        op_arr = [0]*n
+        op_arr = []
         for i in range(n):
             if (i+1) %3 ==0 and (i+1) %5 ==0:
-                op_arr[i] = "FizzBuzz"
+                op_arr.append("FizzBuzz")
             elif (i+1) %3 ==0:
-                op_arr[i] = "Fizz"
+                op_arr.append("Fizz") 
             elif (i+1) %5 ==0:
-                op_arr[i] = "Buzz"
+                op_arr.append("Buzz")
             else:
-                op_arr[i]= str(i+1)
+                op_arr.append(str(i+1))
         return op_arr
 
         
