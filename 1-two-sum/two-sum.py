eb84c69edfb202c103db25272ac004a0414640ac
Mugilan <expectopetronum002@gmail.com>
2026-05-28 19:14:12 +0530

    Time: 3 ms (52.81%) | Memory: 20.5 MB (42.65%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        hashmap = defaultdict(int)
-        for i in range(n):
-            hashmap[nums[i]] = i #2->0 (val to index)
-        print(hashmap.items())
-        for i in range(n):
-            need = target - nums[i] # 9-2 =7
-            if need in hashmap.keys() and hashmap[need]!=i: #element is there but not from same index
-                return[i,hashmap[need]]
+
+        hm = {}
+
+        for i in range(len(nums)):
+            hm[nums[i]] = i
+        
+        for i in range(len(nums)):
+            need = target - nums[i]
+
+            if need in hm.keys() and hm[need]!=i:
+                return [i,hm[need]]
+        return []
 
             
