1d9d30b7d26ffa991747b3a81d94bd0a44c551c3
Mugilan <expectopetronum002@gmail.com>
2026-06-10 10:13:20 +0530

    Time: 30 ms (5.25%) | Memory: 13.5 MB (73.91%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,22 +1,7 @@
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count=0
-        cand= None
-        for x in nums:
-
-            if count==0:
-                cand= x
-                count=1
-            elif x == cand:
-                count+=1
-
-            else:
-                count -=1
-        return cand
+        count = Counter(nums)
+        return max(count, key = count.get)
                 
 
 
