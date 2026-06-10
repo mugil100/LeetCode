daf4801d54915d8ca437aad9630399592ae81f10
Mugilan <expectopetronum002@gmail.com>
2026-06-10 10:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (6.82%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,23 @@
 class Solution(object):
     def majorityElement(self, nums):
-        count = Counter(nums)
-        return max(count, key = count.get)
+        # keep an index for travrsign the array
+        # keep an ele for markign the most repeated value
+        count = 1
+        cand = nums[0]
+
+        if len(nums)<=1:
+            return nums[0]
+
+        for x in range(1,len(nums)):
+            if nums[x] != cand:
+                if count<1:
+                    cand = nums[x]
+                else:
+                    count-=1
+            else:
+                count+=1
+        return cand
+
                 
 
 
